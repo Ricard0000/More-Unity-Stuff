061b6dc3147c12bbc649a6e119c1e1f22bb70fc6
Ricardo Delgadillo (Ph.D.) <72097455+Ricard0000@users.noreply.github.com>
2024-02-22 22:37:38 -0800

    Add files via upload

--- WallEntrance2.cs
+++ WallEntrance2.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WallEntrance2 : MonoBehaviour
 {
 
-
+    public ParticleSystem system;
     //Define public variables
     public float Pos1;//Object position
     public float Pos2;//Object position
@@ -30,6 +30,39 @@ public class WallEntrance2 : MonoBehaviour
     public Material mat;
 
     GameObject gog;// = new GameObject("Plane");
+
+    void Update()
+    {
+        float Pos1 = 0.1825f - 0.0625f;//Object position
+        float Pos2 = -0.221875f - 0.06f;
+        float Pos3 = -0.9275f;//4375f;
+        Vector3 POS;
+        POS = new Vector3(Pos1, Pos2, Pos3);
+
+        //        Vector3 particlePosition = new Vector3(-0.024375f,0.053805460f,-0.092708340f);
+
+        Vector3 particlePosition = new Vector3(-0.243750f,0.37410460f,-0.92708340f);
+
+
+
+
+
+        float time = 0f;
+        float x = .16395000f * Mathf.Cos(time + 0 * PI / 2f);
+        float y = .16395000f * Mathf.Sin(time + 0 * PI / 2f);
+//        new Vector3(-x + .16395000f, y, 0f) + new Vector3(aTemp2[0], aTemp2[1], aTemp2[2]) + POS;
+
+
+
+
+        //        Debug.Log(particlePosition * 100000000);
+        //      Debug.Log(particlePosition * 100000000);
+        //    Debug.Log(particlePosition * 100000000);
+        //  Debug.Log(particlePosition * 100000000);
+        //   system.Emit(new ParticleSystem.EmitParams() { position = particlePosition }, 1);
+    }
+
+
     void Awake()
     {
         gog = MakeDiscreteProceduralGrid(Pos1, Pos2, Pos3, c1, c2, c3, c4, s, t, delta_theta, N, true, mat, rot, Nrot);
@@ -80,38 +113,6 @@ public class WallEntrance2 : MonoBehaviour
         return new Vector3(x, y, z);
     }
 
-    public static Vector3 Arch_eq_6_C(float x, float z, float c1, float c2, float angle)
-    {
-        float y;
-        y = c2 * Mathf.Sqrt(1 - Mathf.Abs(x) / (c1 + 0.00001f));
-        y = y + c2 * Mathf.Sqrt(1 - Mathf.Abs(x * x) / (c1 * c1 + 0.00001f));
-        y = y / 2.0f - 0.00001f;
-
-        float temp = x;
-        x = x * Mathf.Cos(-2 * angle) - z * Mathf.Sin(-2 * angle);
-        z = temp * Mathf.Sin(-2 * angle) + z * Mathf.Cos(-2 * angle);
-        return new Vector3(x, y, z);
-    }
-
-    public static float Vert_Rot_x(float x, float z, float angle)
-    {
-        return x * Mathf.Cos(angle) - z * Mathf.Sin(angle);
-    }
-
-    public static float Vert_Rot_z(float x, float z, float angle)
-    {
-        return x * Mathf.Sin(angle) + z * Mathf.Cos(angle);
-    }
-
-
-    public static float uv_Arch_eq_6(float x)
-    {
-        float y;
-        y = Mathf.Sqrt(1 - Mathf.Abs(x) / (1.0f + 0.00001f));
-        y = y + Mathf.Sqrt(1 - Mathf.Abs((x) * (x)) / (1.0f + 0.00001f));
-        y = y / 2.0f - 0.00001f;
-        return y;
-    }
 
     public static GameObject MakeDiscreteProceduralGrid(float Pos1, float Pos2, float Pos3, float c1, float c2, float c3, float c4, float s, float t, float delta_theta, int N, bool collider, Material mat, int rot, int Nrot)
     {
@@ -172,44 +173,74 @@ public class WallEntrance2 : MonoBehaviour
         vertices[nextVert] = temp3 + POS + new Vector3(0.0625f, .25975f, 0f);//Vert 0
         uvs[0] = new Vector2(vertices[0].x, vertices[0].y);
 
+//        Debug.Log(vertices[nextVert] * 100000000);
+  //      Debug.Log(vertices[nextVert] * 100000000);
+    //    Debug.Log(vertices[nextVert] * 100000000);
+      //  Debug.Log(vertices[nextVert] * 100000000);
+
+
+
+
         nextVert = 1 + nextVert;
         x = 0.175f * c1 * Mathf.Cos(0 * delta_theta * 2 * N * 1f / 8.00000f);
         z = -dz * 4.3333333f - dz * 2.5f;
-        vertices[0 + nextVert] = Arch_eq_6_A(x, z, 0.175f * c1, 0.175f * c2, angle) + POS + new Vector3(-0.525f + 0.0625f, -aFloat + c2 * 0.525f - 0.5f, -8f * dz) + new Vector3(0f, -.060f, 0f);//Vert 1 This changed the Bottom part.Both Triangles?
+        vertices[0 + nextVert] = Arch_eq_6_A(x, z, 0.175f * c1, 0.175f * c2, angle) + POS + new Vector3(-0.525f + 0.0625f, -aFloat + c2 * 0.525f - 0.5f, -8f * dz) + new Vector3(0f, -.060f, 0f);//Vert 1 This changed the Bottom part.Both Triangles? // Not this
+//        vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);                                                                                                                                                                                                 //        vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
 
         nextVert = 1 + nextVert;
-        vertices[0 + nextVert] = 0.5f * (vertices[0] + vertices[1]);//Vert 2
+        vertices[0 + nextVert] = 0.5f * (vertices[0] + vertices[1]);//Vert 2 // Not this
+//        vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
-        vertices[0 + nextVert] = 0.5f * (vertices[0] + vertices[1]) + new Vector3(0.75f,0f,0f);//Vert 3
+        vertices[0 + nextVert] = 0.5f * (vertices[0] + vertices[1]) + new Vector3(0.75f,0f,0f);//Vert 3 Not this
+//vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
-        vertices[0 + nextVert] = vertices[1] + new Vector3(0.75f, 0f, 0f);//Vert 4
+        vertices[0 + nextVert] = vertices[1] + new Vector3(0.75f, 0f, 0f);//Vert 4 Not this
+// vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
-        vertices[0 + nextVert] = 0.6f * vertices[0] + 0.4f * vertices[2];//Vert 5
+        vertices[0 + nextVert] = 0.6f * vertices[0] + 0.4f * vertices[2];//Vert 5 Not this
+//vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-0.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
         Vector3 vert3Temp = vertices[3];
-        vertices[0 + nextVert] = vertices[0] + new Vector3(-0.0625f + vert3Temp[0]-temp3[0],0f,0f);//Vert 6
+        vertices[0 + nextVert] = vertices[0] + new Vector3(-0.0625f + vert3Temp[0]-temp3[0],0f,0f);//Vert 6 Left most upper
+        //
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
-        vertices[0 + nextVert] = 0.6f * vertices[6] + 0.4f * vertices[3];//Vert 7
+        vertices[0 + nextVert] = 0.6f * vertices[6] + 0.4f * vertices[3];//Vert 7 This looks like dead co
+//        vertices[0 + nextVert] = vertices[0 + nextVert] + new Vector3(-1.25f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
 
         Vector3 aTemp1 = vertices[0] - vertices[5];
         Vector3 aTemp2 = vertices[5];
 
+        Debug.Log(aTemp2 * 100000000);
+        Debug.Log(aTemp2 * 100000000);
+        Debug.Log(aTemp2 * 100000000);
+        Debug.Log(aTemp2 * 100000000);
 
-        float radius = Mathf.Abs(aTemp1[1]);
+
+        float radius = Mathf.Abs(aTemp1[1]); // .16395000
+        Debug.Log((radius - radius * 0.8f) * 100000000);
+        Debug.Log((radius - radius * 0.8f) * 100000000);
+        Debug.Log((radius - radius * 0.8f) * 100000000);
+        Debug.Log((radius - radius * 0.8f) * 100000000);
+
+
+        radius = radius * 0.8f;
+
+
+
 
 
         nextVert = 1 + nextVert;
@@ -219,6 +250,7 @@ public class WallEntrance2 : MonoBehaviour
             x = radius * Mathf.Cos(time + 0 * PI / 2f);
             y = radius * Mathf.Sin(time + 0 * PI / 2f);
             vertices[ii + nextVert] = new Vector3(-x+radius, y, 0f) + new Vector3(aTemp2[0], aTemp2[1], aTemp2[2]) + POS;// Right Half of right circle.
+            vertices[ii + nextVert] = vertices[ii + nextVert] + new Vector3(0.0f, 0.03279f, 0f);
             uvs[ii + nextVert] = new Vector2(vertices[ii + nextVert].x, vertices[ii + nextVert].y);
         }
 
@@ -229,25 +261,33 @@ public class WallEntrance2 : MonoBehaviour
             x = radius * Mathf.Cos(time + PI / 2f);
             y = radius * Mathf.Sin(time + PI / 2f);
             vertices[ii + nextVert] = new Vector3(-x + radius, y, 0f) + new Vector3(aTemp2[0], aTemp2[1], aTemp2[2]) + POS;// Left Half of right circle
+            vertices[ii + nextVert] = vertices[ii + nextVert] + new Vector3(-0.03279f, 0.03279f, 0f);
             uvs[ii + nextVert] = new Vector2(vertices[ii + nextVert].x, vertices[ii + nextVert].y);
         }
 
         nextVert = Nrot + nextVert;
         vertices[nextVert] = vertices[0] + new Vector3( 2f * radius , 0f , 0f );
+//        vertices[nextVert] = vertices[nextVert] + new Vector3(0f, 0f, 0f);      // MODDED Not This: Stretches right side of left top circle thing
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
-        vertices[nextVert] = vertices[6] + new Vector3(-1f * 0.0625f, 0f, 0f);
+        vertices[nextVert] = vertices[6] + new Vector3(-1f * 0.0625f, 0f, 0f); // This but there is some dependencies
+        vertices[nextVert] = vertices[nextVert] + new Vector3(-0.125f, 0f, 0f); // This strechces the part in the middel, the pillar part
+        int saveCorrection2 = nextVert;
+
+        //        vertices[nextVert] = vertices[nextVert] + new Vector3(-0.125f, 0f, 0f);
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         nextVert = 1 + nextVert;
         vertices[nextVert] = vertices[3] + new Vector3(-1f * 0.0625f, 0f, 0f);
+        int saveCorrection = nextVert;
+
+//        vertices[nextVert] = vertices[nextVert];// + new Vector3(-0.125f, 0f, 0f);// Part of it 0.125 almost looks spot on
+
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
 
         Vector3 aTemp3 = 0.6f * vertices[2 * Nrot + 9] + 0.4f * vertices[2 * Nrot + 10];
-
-
-
+//        aTemp3 = aTemp3 + new Vector3(-0.003125f, 0f, 0f);                      // MODDED Not this
 
         nextVert = 1 + nextVert;
         for (int ii = 0; ii < Nrot; ii++)
@@ -256,6 +296,7 @@ public class WallEntrance2 : MonoBehaviour
             x = radius * Mathf.Cos(time + 0f * PI / 2f);
             y = radius * Mathf.Sin(time + 0f * PI / 2f);
             vertices[ii + nextVert] = new Vector3(x - radius, y, 0f) + new Vector3(aTemp3[0], aTemp3[1], aTemp3[2]) + POS;// Left Half of right circle.
+            vertices[ii + nextVert] = vertices[ii + nextVert] + new Vector3(-0.03279f * 2f - 0.03f, 0.03279f, 0f);
             uvs[ii + nextVert] = new Vector2(vertices[ii + nextVert].x, vertices[ii + nextVert].y);
         }
 
@@ -267,13 +308,21 @@ public class WallEntrance2 : MonoBehaviour
             x = radius * Mathf.Cos(time + 1f * PI / 2f);
             y = radius * Mathf.Sin(time + 1f * PI / 2f);
             vertices[ii + nextVert] = new Vector3(x - radius, y, 0f) + new Vector3(aTemp3[0], aTemp3[1], aTemp3[2]) + POS;// Right Half of left circle.
+            vertices[ii + nextVert] = vertices[ii + nextVert] + new Vector3(-0.03279f - 0.03f, 0.03279f, 0f);
             uvs[ii + nextVert] = new Vector2(vertices[ii + nextVert].x, vertices[ii + nextVert].y);
         }
 
 
         nextVert = Nrot + nextVert;
         vertices[nextVert] = vertices[2 * Nrot + 9] + new Vector3(-2 * radius, 0f, 0f);
+        vertices[nextVert] = vertices[nextVert] + new Vector3(0.0f, 0f, 0f);         // This is the dependent
         uvs[nextVert] = new Vector2(vertices[nextVert].x, vertices[nextVert].y);
+
+
+        vertices[saveCorrection] = vertices[saveCorrection] + new Vector3(-0.171f,-0f,0f);
+        uvs[saveCorrection] = new Vector2(vertices[saveCorrection].x, vertices[saveCorrection].y);
+        vertices[saveCorrection2] = vertices[saveCorrection2] + new Vector3(-0.0455f, 0f, 0f);
+        uvs[saveCorrection2] = new Vector2(vertices[saveCorrection2].x, vertices[saveCorrection2].y);
 
         int i3 = 0;
         triangles[i3] = 2;
@@ -352,7 +401,7 @@ public class WallEntrance2 : MonoBehaviour
         triangles[nextTri + 1] = 16 + 3 * Nrot;
         triangles[nextTri + 2] = 2 * Nrot + 7;
 
-        Debug.Log(vertices[4]*100000000);
+//        Debug.Log(vertices[4]*100000000);
 
 
         m.vertices = vertices;
